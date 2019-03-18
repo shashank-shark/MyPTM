449e615b1e6137f453c8851d305c625e4d2f28e3
shashank-shark <shashankreddy231@gmail.com>
2019-03-18 23:03:35 +0530

    Update App.js
    Front App.js update.

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Live reload is enabled,\n' +
+    'Use Simple JSX for all.',
 });
 
 type Props = {};
@@ -22,8 +22,9 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>MY-PTM APP</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>Simple App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.bold}>SHASHANK J</Text>
       </View>
     );
   }
@@ -46,4 +47,9 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  bold: {
+    textAlign: 'center',
+    color: '#333333',
+    margin: 10
+  }
 });
