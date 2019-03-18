7c147be54b1f5fce51de35b7aa9db8c89f7d9610
shashank-shark <shashankreddy231@gmail.com>
2019-03-19 01:24:51 +0530

    Create Messages.js
    Sample Messages App.

---
+++ src/components/Messages.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class Messages extends Component {
+    render() {
+        return (
+            <Text>Hello World</Text>
+        );
+    }
+}
