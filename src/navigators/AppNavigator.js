d74d4d028f598261f6e82ebc567b65fdad641499
shashank-shark <shashankreddy231@gmail.com>
2019-03-19 01:25:05 +0530

    Update AppNavigator.js
    Updated Navigator for messages.

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -2,10 +2,12 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import Homescreen from '../components/Homescreen';
 import Events from '../components/Events';
+import Messages from '../components/Messages';
 
 const App = createStackNavigator({
     Homescreen: { screen: Homescreen },
     Events: { screen: Events },
+    Messages : { screen : Messages },
 });
 
 const AppNavigator = createAppContainer(App);
