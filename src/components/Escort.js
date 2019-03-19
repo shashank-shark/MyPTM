7ee1281d355d401bcc9e90ace3fdcaed1289ddbf
shashank-shark <shashankreddy231@gmail.com>
2019-03-19 21:42:52 +0530

    Create Escort.js
    Escort screen.

---
+++ src/components/Escort.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class Escort extends React.Component {
+    render () {
+        return (
+            <View>
+                <Text>Escort</Text>
+            </View>
+        );
+    }
+}
