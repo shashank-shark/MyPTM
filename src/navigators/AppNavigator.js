15d2df0516f725102ad9c17752bc273f75c95241
shashank-shark <shashankreddy231@gmail.com>
2019-03-19 21:43:25 +0530

    Update AppNavigator.js
    Modified AppNavigator file.

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -3,11 +3,13 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Homescreen from '../components/Homescreen';
 import Events from '../components/Events';
 import Messages from '../components/Messages';
+import Escort from '../components/Escort';
 
 const App = createStackNavigator({
     Homescreen: { screen: Homescreen },
     Events: { screen: Events },
     Messages : { screen : Messages },
+    Escort: {screen : Escort},
 });
 
 const AppNavigator = createAppContainer(App);
