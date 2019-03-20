553d8d8bded35017dc36ecb6a4ecfcc5a6b4ecf9
shashank-shark <shashankreddy231@gmail.com>
2019-03-20 17:31:19 +0530

    Create Loading.js
    Created loading screen for auth.

---
+++ src/Authflow/Loading.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Loading extends Component {
+    render () {
+        return (
+            <View style={styles.container}>
+            <Text>Loading</Text>
+            <ActivityIndicator size="large" />
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
+  })
